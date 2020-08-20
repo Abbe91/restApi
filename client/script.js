425b5cd398c9de5558cf4e3f2d4c79fafda487ea
Abdullah <Abdullah@zenio.se>
2020-08-20 09:48:40 +0200

    Push the projekt

---
+++ client/script.js
@@ -0,0 +1,76 @@
+
+window.addEventListener('load', start)
+
+function othername() {
+    var input = document.getElementById("userInput").value;
+    alert(input);
+}
+async function start(){
+    //try catch testa
+    const result = await fetch('/users')
+    const users =  await result.json()
+    let contant = document.getElementById("outPUt");
+    for (let i = 0; i < users.length; i++) {
+        console.log(users)
+        let name = (users[i].name)
+        let div = document.createElement("div")
+        let p = document.createElement("p")
+        p.innerText = name
+        div.appendChild(p)
+        contant.appendChild(div)
+    } 
+ };
+
+async function createTodo(event){
+    event.preventDefuault()
+
+    event.target
+};
+
+function getAll(){
+    console.log("getAll")
+    makeRequest("/users", "get")
+    location.reload()
+//  let coloectUsers = makeRequest("/users", "get") 
+    
+}
+
+function getSpecific(id){
+    console.log("getSpecific", id)
+    makeRequest("/users/" + 0, "get")
+}
+function addNew(){
+    var inputName = document.getElementById("userInput").value;
+    let body = {
+        id:"",
+        name: inputName,
+        isStudent: false
+    }
+    makeRequest("/users", "post", JSON.stringify(body))
+}
+function update(id){
+    let body = {
+        id:"_l7otr0fg5",
+        name: "Yassen",
+        isStudent: true   
+    }
+    makeRequest("/users/_l7otr0fg5", "put", JSON.stringify(body))
+}
+
+function remove(id){
+    console.log("remove", id)
+    makeRequest("/users/1" , "delete")
+}
+
+async function makeRequest(url, reqMethod, body ){
+    const response = await fetch(url, {
+        headers: {"content-type": "application/json"}, 
+        method : reqMethod,
+        body: body
+    })
+    console.log(response)
+    // location.reload()
+    const data = await response.json()
+    console.log(data)
+    return data
+}
---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require('express');
+ function getId() {
+    // Math.random should be unique because of its seeding algorithm.
+    // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+    // after the decimal.
+    return '_' + Math.random().toString(36).substr(2, 9);
+};
+//define a state for our data (database)
+let idIndex = 100
+const users = [{
+    id:"_l7otr0fg5",
+    name: 'Abbe',
+    isStudent: true
+}]
+const app = express()
+
+// make sure body is parsed as Json
+app.use(express.json())
+
+
+
+//make sure client folder is accessable 
+app.use(express.static('./client'))
+
+
+//Defined all Todo resource Endpoints
+app.get('/users', (req, res)=> res.json(users))
+
+app.get('/users/:id', (req, res)=> res.json('One user, id: '+ req.params.id))
+
+app.post('/users', (req, res)=> {
+    if(!req.body.id || !req.body.name){
+        res.status(400).json({message: 'Data sent as body is not correct'})
+    }else{
+
+    const todo = { id: getId(), ...req.body }
+    todo.id=getId()
+    users.push(todo);
+    res.status(201).json(todo)
+    idIndex++;
+    }
+})
+
+app.put("/users/:id" , (req , res) => {
+    const paramId = req.params.id 
+    let foundUserIndex = users.findIndex((user) => user.id == paramId)
+    
+    if (foundUserIndex == -1){
+    res.status(404).json({status: "User to update not found...."})
+    }
+    
+    users[foundUserIndex] = req.body
+    res.json({status: " User updated"})
+   })
+
+
+app.delete("/users/:id", [checkIfUserExists], (req,res) => {
+    users.splice(req.foundUserIndex, 1)
+    res.json({status: "User deleted"})
+})
+
+function checkIfUserExists (req, res, next){
+    const paramId = req.params.id
+
+    let foundUserIndex = users.findIndex((user) =>user.id == paramId)
+    
+
+    if(foundUserIndex == -1){
+        res.status(404).json({status: "User not found..."})
+    }else {
+        req.foundUserIndex = foundUserIndex
+        next()
+    }
+
+}
+
+
+
+//404  middleware
+app.use((req, res)=>{
+    res.status(404).json(
+        
+        {
+             message: 'Could not find the resource you are looking for..'
+        })
+})
+
+//start th server
+app.listen(3000, () => console.log('Server is up'))
